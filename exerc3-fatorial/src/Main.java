20e849bb098ec307a2944b7665012f3e889d9bf7
Diego <diego.yeu@gmail.com>
2025-03-28 15:26:48 -0300

    Exercicio 3 - calcular fatorial

--- exerc3-fatorial/src/Main.java
+++ exerc3-fatorial/src/Main.java
@@ -4,9 +4,23 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int numero = 0;
+        boolean valorPermitido = false;
 
-        System.out.println("Digite um número inteiro para calcular o fatorial:");
-        int numero = scanner.nextInt();
+        while (!valorPermitido) {
+            System.out.println("Digite um número inteiro para calcular o fatorial:");
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+                if (numero >= 0) {
+                    valorPermitido = true;
+                } else {
+                System.out.println("Por favor digitar um número inteiro");
+                }
+            } else {
+                System.out.println("Valor inválido! Por favor digitar um número inteiro");
+                scanner.next();
+            }
+        }
 
         long resultadoFatorial = Fatorial.calcularFatorial(numero);
         System.out.printf("%d! = %d \n", numero, resultadoFatorial);
